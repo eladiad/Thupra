9cb798a0059ea20a120e74c45064ae8d9b129c17
eladiad <eladiadsadiku@gmail.com>
2019-06-23 18:31:14 +0200

    eladiad
    done for today dtnm
    dmth diten e mire

--- Thupra Website/js/textChangeBreakLine.js
+++ Thupra Website/js/textChangeBreakLine.js
@@ -2,20 +2,17 @@ $(document).ready( function() {
     /* Check width on page load*/
     if ( $(window).width() < 960) {
         $(".p1").addClass("breakLine");
-    $('.breakLine > span:not(:last-of-type)').before('<br/>');
-
+        $('.breakLine > span:not(:last-of-type)').before('<br/>');
     }
-    else {}
+    else {
+        $(".p1").removeClass("breakLine");
+    }
  });
 
  $(window).resize(function() {
     /*If browser resized, check width again */
     if ($(window).width() < 960) {
         $(".p1").addClass("breakLine");
-    $('.breakLine > span:not(:last-of-type)').before('<br/>');
-        
     }
-    else {$(".p1").removeClass("breakLine");
-    $('.breakLine > span:not(:last-of-type)').removeAttr('<br/>');
-}
  });
+ $('.breakLine > span:not(:last-of-type)').before('<br/>');
