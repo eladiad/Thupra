a97102d9d387d93abfc388857101e09dd0525dd3
jetonhaxha <33187356+jetonhaxha@users.noreply.github.com>
2019-06-23 18:22:31 +0200

    jeton

---
+++ Thupra Website/js/textChangeBreakLine.js
@@ -0,0 +1,21 @@
+$(document).ready( function() {
+    /* Check width on page load*/
+    if ( $(window).width() < 960) {
+        $(".p1").addClass("breakLine");
+    $('.breakLine > span:not(:last-of-type)').before('<br/>');
+
+    }
+    else {}
+ });
+
+ $(window).resize(function() {
+    /*If browser resized, check width again */
+    if ($(window).width() < 960) {
+        $(".p1").addClass("breakLine");
+    $('.breakLine > span:not(:last-of-type)').before('<br/>');
+        
+    }
+    else {$(".p1").removeClass("breakLine");
+    $('.breakLine > span:not(:last-of-type)').removeAttr('<br/>');
+}
+ });
