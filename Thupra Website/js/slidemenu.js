d125bd416e78c36aa46be13e49bfdc5d97ffbc22
eladiad sadiku <eladiadsadiku@hotmail.de>
2019-12-11 20:07:52 +0100

    side nav
    navbar changed from left to right

--- Thupra Website/js/slidemenu.js
+++ Thupra Website/js/slidemenu.js
@@ -1,11 +1,15 @@
 function openSlideMenu(){
     document.getElementById('side-menu').style.width = '250px';
-    document.getElementById('thupraGrid').style.marginLeft = '250px';
+    document.getElementById('thupraGrid').style.marginLeft = '-250px';
+    document.getElementById('header').style.marginLeft = '250px';
     document.getElementById('Side').style.width = '100vw';
+    document.getElementById('nav-icon').style.display = 'none';
 }
 function closeSlideMenu(){
     document.getElementById('side-menu').style.width = '0';
     document.getElementById('thupraGrid').style.marginLeft = '0';
+    document.getElementById('header').style.marginLeft = '0';
     document.getElementById('Side').style.width = '0';
+    document.getElementById('nav-icon').style.display = 'block';
     $('#nav-icon').toggleClass('open');
 }
