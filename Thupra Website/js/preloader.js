3888d501db5e4d2d11ec7777deec900f249b7e30
eladiad sadiku <eladiadsadiku@hotmail.de>
2019-12-19 21:41:58 +0100

    es

--- Thupra Website/js/preloader.js
+++ Thupra Website/js/preloader.js
@@ -1,17 +1,4 @@
 $(document).ready(function() {
-    var floatingImg = document.getElementById("floatingImg");
-    var main__floating__img = document.getElementById('main__floating__img');
-    var rendomNumberForFloatingImg = Math.floor(Math.random() * 3) + 1;
-    floatingImg.src = 'img/Main Photo/Device-' + rendomNumberForFloatingImg + '.png';
-    if(rendomNumberForFloatingImg == 2){
-        main__floating__img.style.transform = 'rotate(400deg)';
-    }
-    else if(rendomNumberForFloatingImg == 3){
-        main__floating__img.style.transform = 'rotate(30deg)';
-    }
-    else{
-        
-    }
 //Preloader
 $(window).on("load", function() {
     preloaderFadeOutTime = 500;
