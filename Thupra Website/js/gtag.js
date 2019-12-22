3e814d553a549a4fcfe4940128f3d1b25bf36667
eladiad sadiku <eladiadsadiku@hotmail.de>
2019-12-22 09:48:10 +0100

    es

---
+++ Thupra Website/js/gtag.js
@@ -0,0 +1,7 @@
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-136590539-2');
+  gtag('set', {'user_id': 'ladigo1.'}); // Set the user ID using signed-in user_id.
+  ga('set', 'userId', 'ladigo1.'); // Set the user ID using signed-in user_id.
